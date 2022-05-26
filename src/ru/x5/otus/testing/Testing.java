4006a343c6d34a877cb60c28f5d2b9392b007fea
GeorgyManokhin <zenden100@mail.ru>
2022-05-26 17:41:26 +0400

    test git commit

--- src/ru/x5/otus/testing/Testing.java
+++ src/ru/x5/otus/testing/Testing.java
@@ -2,6 +2,7 @@ package ru.x5.otus.testing;
 
 import java.util.Scanner;
 
+
 public class Testing {
     public static void main(String[] args) {
 
@@ -12,19 +13,43 @@ public class Testing {
         questions[1] = "Столица Сербии?";
         questions[2] = "Столица Бразилии?";
 
-        String[][] answerOptions = {
-         {"Москва", "Сочи", "Казань", "Пермь"},
-         {"Приштина", "Белград", "Ниш", "Крагуевац"},
-         {"Форталеза", "Салвадор", "Куритиба", "Бразилиа"}
-        };
+        String[][] answerOptions = new String[3][4];
+        answerOptions[0][0] = "1. Москва ";
+        answerOptions[0][1] = "2. Сочи ";
+        answerOptions[0][2] = "3. Казань ";
+        answerOptions[0][3] = "4. Пермь ";
 
-        int[] correctAnswers = null;
+        answerOptions[1][0] = "1. Приштина ";
+        answerOptions[1][1] = "2. Белград ";
+        answerOptions[1][2] = "3. Ниш ";
+        answerOptions[1][3] = "4. Крагуевац ";
 
-        for (int i = 0; i < 3; i++) {
+        answerOptions[2][0] = "1. Форталеза ";
+        answerOptions[2][1] = "2. Салвадор ";
+        answerOptions[2][2] = "3. Куритиба ";
+        answerOptions[2][3] = "4. Бразилиа ";
+
+        int[] correctAnswers = new int[3];
+        correctAnswers[0] = 1;
+        correctAnswers[1] = 2;
+        correctAnswers[2] = 4;
+
+
+        for (int i = 0; i < 3; i++) { //i=1
             System.out.println(questions[i]);
+            for (int d = 0; d < answerOptions[i].length; d++) { //d=0 answerOptions[0].length = 4
+                String n = answerOptions[i][d];
+                System.out.println(n + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Результат: правильно "+correctCount +", неправильно "+wrongCount);
     }
 }
-}
+
+
+
+
 
 
 
