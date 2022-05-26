a3ced806d18205d16b5fd96ef87d8ffe3d74aa68
GeorgyManokhin <zenden100@mail.ru>
2022-05-26 05:34:03 +0400

    test git commit

--- src/ru/x5/otus/testing/Testing.java
+++ src/ru/x5/otus/testing/Testing.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) {
+
+        int correctCount = 0, wrongCount = 0;
+
         String[] questions = new String[3];
         questions[0] = "Столица России?";
         questions[1] = "Столица Сербии?";
         questions[2] = "Столица Бразилии?";
 
-        String[][] answerOptions = null;
+        String[][] answerOptions = {
+         {"Москва", "Сочи", "Казань", "Пермь"},
+         {"Приштина", "Белград", "Ниш", "Крагуевац"},
+         {"Форталеза", "Салвадор", "Куритиба", "Бразилиа"}
+        };
+
+        int[] correctAnswers = null;
 
         for (int i = 0; i < 3; i++) {
             System.out.println(questions[i]);
