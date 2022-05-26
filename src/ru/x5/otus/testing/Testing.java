a5bf1bbbdbbf94ad5f6f0f11c02b144ac9b92dbd
GeorgyManokhin <zenden100@mail.ru>
2022-05-26 05:07:25 +0400

    test git commit

---
+++ src/ru/x5/otus/testing/Testing.java
@@ -0,0 +1,21 @@
+package ru.x5.otus.testing;
+
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) {
+        String[] questions = new String[3];
+        questions[0] = "Столица России?";
+        questions[1] = "Столица Сербии?";
+        questions[2] = "Столица Бразилии?";
+
+        String[][] answerOptions = null;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(questions[i]);
+    }
+}
+}
+
+
+
